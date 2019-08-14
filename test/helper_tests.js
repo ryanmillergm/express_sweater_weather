173b41c123f25d6dbca37f9d7a6d87e160bbb2fe
Ryan Miller <ryanmillergm@gmail.com>
2019-08-14 08:32:01 -0600

    Refactored test files

--- test/helper_tests.js
+++ test/helper_tests.js
@@ -1,25 +1,14 @@
-var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+before((done) => {
+  mongoose.connect('mongodb://localhost/express_sweater_weather', { useNewUrlParser: true });
+  mongoose.connection
+      .once('open', () => {
+        // console.log('Connected!'))
+      done();
+    })
+      .on('error', error => {
+          console.warn('Error : ',error);
     });
-  });
 });
-
-
-// const mongoose = require('mongoose');
-//
-// mongoose.Promise = global.Promise;
-// mongoose.connect('mongodb://localhost/express_sweater_weather');
-// mongoose.connection
-//     .once('open', () => console.log('Connected!'))
-//     .on('error', (error) => {
-//         console.warn('Error : ',error);
-//     });
-//
-// beforeEach((done) => {
-//     mongoose.connection.collections.pokemons.drop(() => {
-//         done();
-//     });
-// });
