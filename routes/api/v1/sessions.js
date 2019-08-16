fdbace67d6281531a7e12e1855d0e66124685ec1
Ryan Miller <ryanmillergm@gmail.com>
2019-08-15 18:02:20 -0600

    Adds user login with sessions

--- routes/api/v1/sessions.js
+++ routes/api/v1/sessions.js
@@ -1,6 +1,7 @@
 let UserModel = require('../../../models/user.model');
 var express = require('express');
 var router = express.Router();
+var bcrypt = require('bcrypt')
 
 router.post('/', (req, res, next) => {
   UserModel.findOne(
@@ -11,10 +12,14 @@ router.post('/', (req, res, next) => {
    }
 )
   .then(user => {
-    res.setHeader("Content-Type", "application/json");
-    res.status(200).send(JSON.stringify({
-      "api_key": `${user.api_key}`
-      }));
+    if (bcrypt.compareSync(req.body.password, user.password)) {
+      res.setHeader("Content-Type", "application/json");
+      res.status(200).send(JSON.stringify({
+        "api_key": `${user.api_key}`
+        }));
+    } else {
+      res.status(404).send("Incorrect email or password")
+    }
   })
   .catch(error => {
     res.setHeader("Content-Type", "application/json");
